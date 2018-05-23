e8a488e613d2a8efa850f65c57e6a35937d462a4
Piyapong Kaewnan <mailtopiyapong@gmail.com>
2018-05-23 14:08:51 +0700

    Add Customer Detail

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,6 +59,7 @@
                             @else
                             <li><a href="{{ url('/typebooks') }}"><i class="fa fa-book"></i> TypeBook</a></li>
                             <li><a href="{{ url('/books') }}"><i class="fa fa-book"></i> Book</a></li>
+                            <li><a href="{{ url('/customers') }}"><i class="fa fa-users"></i> Customers</a></li>
                             <li><a href="{{ url('/about')}}"><i class="fa fa-edit"></i> About</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
